24b840b7708210946d727d1655931f342ec02417
dakaganeshreddy <dakaganesh@gmail.com>
2018-10-15 18:43:03 +0530

    hi git

---
+++ MavenCucumber/src/main/java/Cucumber_day5/MavenCucumber/App.java
@@ -0,0 +1,13 @@
+package Cucumber_day5.MavenCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
